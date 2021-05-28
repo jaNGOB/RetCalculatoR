d423da1b9dad8f132db784d842504548e036db11
GeorgeKranky <82184128+GeorgeKranky@users.noreply.github.com>
2021-05-28 08:48:47 +0200

    Update plots.R

--- plots.R
+++ plots.R
@@ -15,3 +15,15 @@ ggplot(data = df4, aes(x = items.timestamp, y = items.views)) +
        y = "Total Views",
        title = "German Page Views: LGBT Parenting",
        subtitle = "Daily Page Views")
+
+
+df6<-melt(df5, id.vars ="Date", variable.name = "series")
+
+ggplot(df6, aes(Date, value))+geom_line(aes(colour= series)) + 
+    labs(x = "Date",
+       y = "Total Views",
+       title = "German Page Views: Sanna Marin, Women in Government, Same-Sex Marriage and 
+LGBT Parenting",
+       subtitle = "Daily Page Views")
+
+
