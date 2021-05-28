527e26ccebc33ee21826ddc78683617919324af0
J. Gobeli <gobeli@hotmail.ch>
2021-05-28 10:15:57 +0200

    Update plots.R

--- plots.R
+++ plots.R
@@ -27,3 +27,18 @@ LGBT Parenting",
        subtitle = "Daily Page Views")
 
 
+
+# Plot Returns and Drawdown of the strategy
+
+dd <- drawdown(ret)
+comb <- data.frame((ret-1), dd)
+colnames(comb) <- c("returns", "drawdowns")
+comb['date'] <- as.Date(row.names(comb))
+colors <- c("Drawdowns" = "steelblue", "Returns" = "orange")
+
+ggplot(comb, aes(x=date)) + 
+  geom_area(aes(y = drawdowns, color = "Drawdowns"), fill = "steelblue", size = 1, alpha = 0.4) +
+  geom_line(aes(y = returns, color = "Returns"), size = 1) +
+  labs(y = "Change in percentage", x = "Date", title = "Strategy Return and Drawdown") +
+  scale_color_manual(values = colors) + 
+  theme(legend.title=element_blank())
