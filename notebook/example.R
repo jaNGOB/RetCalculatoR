33096a27acf4321448c4bf343ec0bdb1d5604359
J. Gobeli <gobeli@hotmail.ch>
2021-05-31 18:12:46 +0200

    Add files via upload

--- notebook/example.R
+++ notebook/example.R
@@ -34,21 +34,3 @@ sharpe(df$strat_return)
 ret_dd_plot(returns(df$strat_return), TRUE, df$price)
 
 generate_report(df$strat_return)
-
-density_plot_2(df$strat_return)
-
-
-density_plot_2 <-
-  function(chg){
-    colnames(chg) <- "pct_returns"
-    
-    out <- ggplot2::ggplot(chg, ggplot2::aes(x=pct_returns)) +
-      ggplot2::geom_histogram(ggplot2::aes(y=..density..), colour="black", fill="steelblue", binwidth=0.005, alpha=0.6)+
-      ggplot2::geom_density(alpha=0.25, fill="black")+
-      ggplot2::geom_vline(ggplot2::aes(xintercept=mean(pct_returns)), color="orange",
-                    linetype="dashed", size=1)+
-      ggplot2::labs(title="Distribution of Daily Returns", x="", y = "Density")+
-      ggplot2::scale_x_continuous(labels=scales::percent)
-    return(out)
-  }
-
