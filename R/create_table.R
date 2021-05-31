8e79a52cd7a9c2efd08c03f601ceddc60e32819c
J. Gobeli <gobeli@hotmail.ch>
2021-05-31 18:35:04 +0200

    Add files via upload

--- R/create_table.R
+++ R/create_table.R
@@ -3,7 +3,7 @@ function(chg, ret){
   
   fun <- c("Total Returns", "CAGR", "Max. Drawdown", "Sharpe Ratio", "Sortino Ratio", 
            "Adj. Sortino Ratio", "Best month", "Worst month", "Average winning month", "Average losing month")
-  res <- c(1-as.numeric(ret[length(ret)]), cagr(ret), maxdrawdown(ret), sharpe(chg), 
+  res <- c(as.numeric(ret[length(ret)]), cagr(ret), maxdrawdown(drawdown(ret)), sharpe(chg), 
            sortino(chg), adj_sortino(chg), best(chg, "monthly"), worst(chg, "monthly"), 
            avg_win(chg, "monthly"), avg_loss(chg, "monthly"))
   
--- R/generate_report.R
+++ R/generate_report.R
@@ -1,34 +1,38 @@
 generate_report <-
 function(chg){
-    ret <- returns(chg)
-    text <- "
-  ---
-  title: 'Strategy Report'
-  output: 
-    html_document
-  fontsize: 12pt
-  ---
+  ret <- returns(chg)
+  yaml_header <- "
+---
+title: 'Strategy Report'
+output: 
+  html_document
+fontsize: 12pt
+---"
+  text <- "
+
+```{r, fig.width=8, fig.height=4, echo=FALSE}
+ret_dd_plot(ret)
+yret <- yearly_return(chg)
+tab <- create_table(chg, ret)
+den <- density_plot(chg)
+gridExtra::grid.arrange(
+  grobs = list(yret, tab, den),
+  layout_matrix = rbind(c(1, 2),
+                        c(3, 2))
+)
+create_heatmap(chg)
+```{r, echo=TRUE}
+# Thank you <3
+      "
+  rmd_file_name <- "temp.Rmd"
   
-  ```{r, fig.width=8, fig.height=4, echo=FALSE}
-  ret_dd_plot(ret)
-  yret <- yearly_return(chg)
-  tab <- create_table(chg, ret)
-  den <- density_plot(chg)
-  gridExtra::grid.arrange(
-    grobs = list(yret, tab, den),
-    layout_matrix = rbind(c(1, 2),
-                          c(3, 2))
-  )
-  create_heatmap(chg)
-  ```{r, echo=TRUE}
-  # Thank you <3
-  "
-    rmd_file_name <- "temp.Rmd"
-    
-    content <- paste0("\n", text)
-    
-    write(content, rmd_file_name)
-    
-    rmarkdown::render(rmd_file_name)
-    utils::browseURL(paste0("file://", utils::URLencode(gsub("Rmd$", "html", rmd_file_name))))
-  }
+  content <- paste0(yaml_header,
+                    "\n", 
+                    "\n", 
+                    text)
+  
+  write(content, rmd_file_name)
+  
+  rmarkdown::render(rmd_file_name)
+  #utils::browseURL(paste0("file://", utils::URLencode(gsub("Rmd$", "html", rmd_file_name))))
+}
